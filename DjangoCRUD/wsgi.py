0eddf050333c1f12378dc57115c896847bef4a3d
Ashish Nagar <ashishnagar31@gmail.com>
2016-02-04 00:45:36 +0530

    wsgi updated for apache

--- DjangoCRUD/wsgi.py
+++ DjangoCRUD/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoCRUD.settings")
